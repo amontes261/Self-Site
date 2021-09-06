a29f733fbe18fb9acc7710c69a3d9c955f0a9315
Alex Montes <amontes261@icloud.com>
2021-09-06 02:54:25 -0400

    Trying to fix white screen...
    - fix typos in App.js too

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header container">
         <div className="textContainer">
           <code className= "comingSoonHeader"> Coming Soon </code>
-          <div> Welcome to Alex Montes' website. </div>
-          <div> Thanks for coming, but you're a bit erarly. This webpage is still under development. </div>
+          <div> Hi, I'm Alex's website. </div>
+          <div> Thanks for coming, but you're a bit early. This webpage is still under development. </div>
           <div> Check back soon- renovations could be done by the next time you visit. </div>
         </div>
       </header>
