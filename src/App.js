aa3a58a813292b9a8b1f1dd43b448450f7f34624
Alex Montes <amontes261@icloud.com>
2021-09-06 04:33:38 -0400

    Resolved white screen and finalized coming soon screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import { Card } from '@material-ui/core';
+// import { HashRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header container">
-        <div className="textContainer">
-          <code className= "comingSoonHeader"> Coming Soon </code>
-          <div> Hi, I'm Alex's website. </div>
-          <div> Thanks for coming, but you're a bit early. This webpage is still under development. </div>
-          <div> Check back soon- renovations could be done by the next time you visit. </div>
-        </div>
+        <Card className= "cardContainer bg-dark">
+          <div className="textContainer">
+            <code className= "comingSoonHeader"> Coming Soon </code>
+            <div className= "smallTextContainer">
+              <div class= "pt-5"> Hi, I'm Alex's website. </div>
+              <div> Thanks for coming, but you're a bit early. This webpage is still under development. </div>
+              <div> Check back soon- renovations could be done by the next time you visit. </div>
+            </div>
+          </div>
+        </Card>
       </header>
     </div>
   );
